025c8ac5eae98bc94e4d1f2d413703bc1519397b
xenakios <xenakios@gmail.com>
2017-05-18 16:33:27 +0300

    Remove license text from file

--- Source/breakpoint_envelope.h
+++ Source/breakpoint_envelope.h
@@ -1,20 +1,3 @@
-/*
-This file is part of CDP Front-end.
-
-CDP front-end is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-CDP front-end is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with CDP front-end.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #ifndef JCDP_ENVELOPE_H
 #define JCDP_ENVELOPE_H
 
